df89ac7c1381cd7f78130cadbc26e9892462292e
OogleGlu <avirukbasak@gmail.com>
2022-01-07 01:55:08 +0530

    added other operations

--- src/eval.c
+++ src/eval.c
@@ -3,15 +3,22 @@ void operate(char* result, size_t len, char op, double num1, double num2)
     switch (op)
     {
         case '^':
+            snprintf(result, len, RSLT_ACCURACY_FORM, num1 ^ num2);
             break;
         case '/':
+            snprintf(result, len, RSLT_ACCURACY_FORM, num1 / num2);
+            break;
+        case '%':
+            snprintf(result, len, RSLT_ACCURACY_FORM, num1 % num2);
             break;
         case '*':
+            snprintf(result, len, RSLT_ACCURACY_FORM, num1 * num2);
             break;
         case '+':
-            snprintf(result, len, "%.5lf", num1 + num2);
+            snprintf(result, len, RSLT_ACCURACY_FORM, num1 + num2);
             break;
         case '-':
+            snprintf(result, len, RSLT_ACCURACY_FORM, num1 - num2);
             break;
         default:
             printf("aritheval: unrecognised operator: %c\n", op);
