223e2d8a7819c44d50a008c14af2c0eae9df7edf
OogleGlu <avirukbasak@gmail.com>
2022-01-06 20:57:29 +0530

    bugfixes in nextToken()

--- src/main.c
+++ src/main.c
@@ -5,52 +5,74 @@
 
 # define CH_NULL (char) 0
 # define STRLEN 2049
+# define STRCPLEN (STRLEN - 1)
 
 # define INVAL_TOKEN 0
 # define CHAR_TOKEN 1
 # define INT_TOKEN 2
 
+typedef char *string;
 typedef char byte;
 
 const char STR_NULL[STRLEN] = { CH_NULL };
-const char* OPERATORS = "^/*+-";
-const char* VALID_CHARS = "0123456789( .^/*+-)";
+const char *OPERATORS = "^/*+-";
+const char *VALID_CHARS = "0123456789( .^/*+-)";
 
-const char EXPRESSION[STRLEN] = { CH_NULL };
-const char STR_TOKEN[STRLEN] = { CH_NULL };
+char EXPRESSION[STRLEN] = { CH_NULL };
+char STR_TOKEN[STRLEN] = { CH_NULL };
 
 /**
  * @brief Returns type of next token and sets STR_TOKEN to that token.
  * 
- * @param posn Posn from where to look for token
+ * @param posn Posn From where to look for tokens, gets updated.
  * @return byte -- Type of token
  */
-byte nextToken(const size_t posn)
+byte nextToken(size_t *posn)
 {
-    char c = EXPRESSION[posn];
+    STR_TOKEN[0] = 0;
+    byte token_type = INVAL_TOKEN;
+    size_t len = strlen(EXPRESSION);
+
+    // get character
+    char c = EXPRESSION[*posn];
+
+    // if character isn't a digit
     if (!isdigit(c))
     {
-        const char* ptr = strchr(VALID_CHARS, c);
+        // if character is present in VALID_CHARS, ptr is non-NULL
+        char *ptr = strchr(VALID_CHARS, c);
         if (ptr)
         {
-            strncpy(STR_TOKEN, ptr, 1);
-            return CHAR_TOKEN;
+            // if ptr is non-NULL, copy character to STR_TOKEN and return
+            STR_TOKEN[0] = *ptr;
+            STR_TOKEN[1] = 0;
+            (*posn)++;
+            token_type = CHAR_TOKEN;
         }
-        else
-            return INVAL_TOKEN;
+        else {
+            printf("aritheval: invalid character at posn: %ld\n", *posn);
+        }
     }
     else
     {
-        strncpy(STR_TOKEN, STR_NULL, STRLEN);
-        for (size_t i = posn; ; i++)
+        // while *posn < len, Loop through the EXPRESSION
+        for (size_t i = 0; ; i++, (*posn)++)
         {
-            char c = EXPRESSION[i];
+            char c = EXPRESSION[*posn];
             if (isdigit(c))
+                // if c is a digit, concatenate it to STR_TOKEN
                 strncat(STR_TOKEN, &c, 1);
             else
-                return INT_TOKEN;
+            {
+                // return INT_TOKEN code when digits are finished
+                token_type = INT_TOKEN;
+                STR_TOKEN[i] = 0;
+                break;
+            }
         }
     }
+    // printf("> %s\n", STR_TOKEN);
+    return token_type;
 }
 
 /**
@@ -65,11 +87,36 @@ double eval(const size_t init, const size_t final, byte op)
     double num1 = 0.0;
     double num2 = 0.0;
     double rslt = 0.0;
-    
+
+    size_t len = strlen(EXPRESSION);
+    char *posn = EXPRESSION;
+
+    // Loop through the entire input
+    for (size_t i = 0; i < len; ) {
+        size_t posn = i;
+        byte token_type = nextToken(&posn);
+        switch (token_type) {
+            case INT_TOKEN:
+                printf("N:%s\n", STR_TOKEN);
+                break;
+            case CHAR_TOKEN:
+                printf("O:%s\n", STR_TOKEN);
+                break;
+            case INVAL_TOKEN:
+                exit(4);
+                break;
+            default:
+                printf("aritheval: logical error\n"
+                       "token_type = %d\n"
+                       "STR_TOKEN  = %s\n", token_type, STR_TOKEN);
+                break;
+        }
+        i = posn;
+    }
     return 0.0;
 }
 
-int main(const int argc, const char* const argv[])
+int main(const int argc, const char *const argv[])
 {
     if (argc < 2)
     {
@@ -79,16 +126,17 @@ int main(const int argc, const char* const argv[])
     }
     else if (argc > 2)
     {
-        printf("%s\n", "aritheval: too many arguments provided");
+        printf("aritheval: too many arguments provided\n"
+               "USAGE: aritheval \"[expression]\"\n");
         return 2;
     }
-    else if (strlen(argv[1]) > STRLEN)
+    else if (strlen(argv[1]) > STRCPLEN)
     {
-        printf("%s\n", "aritheval: input exceeded limit 2KB");
+        printf("aritheval: input exceeded limit of %d bytes\n", STRLEN);
         return 3;
     }
-    strncpy((char*)EXPRESSION, argv[1], STRLEN);
+    strncpy(EXPRESSION, argv[1], STRCPLEN);
     const double result = eval(0, strlen(EXPRESSION), 3);
-    printf("Result = %.5lf", result);
+    printf("Result = %.5lf\n", result);
     return 0;
 }
