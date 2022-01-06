0959e7e61db1a929c55071a521db5edb64c730b0
OogleGlu <avirukbasak@gmail.com>
2022-01-07 01:54:46 +0530

    added % operator for modulus

--- src/globalvars.c
+++ src/globalvars.c
@@ -1,6 +1,6 @@
 const char STR_NULL[STRLEN] = { CH_NULL };
-const char *OPERATORS = "^/*+-";
-const char *VALID_CHARS = "0123456789( .^/*+-)";
+const char *OPERATORS = "^/%*+-";
+const char *VALID_CHARS = "0123456789( .^/%*+-)";
 
 char EXPRESSION[STRLEN] = { CH_NULL };
 char STR_TOKEN[STRLEN] = { CH_NULL };
