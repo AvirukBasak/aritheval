e05fa4d99ae3f6a7690d663c8c81b7dc5f5932b4
OogleGlu <avirukbasak@gmail.com>
2022-01-07 14:30:07 +0530

    n/a

--- src/globalvars.c
+++ src/globalvars.c
@@ -4,7 +4,7 @@ const char *VALID_CHARS = "0123456789( .^/%*+-)";
 
 char EXPRESSION[STRLEN] = { CH_NULL };
 char STR_TOKEN[TOKEN_LEN] = { CH_NULL };
-char STR_TOKENS[TOKENS][STRLEN] = { { CH_NULL } };
+char STR_TOKENS[TOKENS][TOKEN_LEN] = { { CH_NULL } };
 
 byte OP_QUEUE[QUEUE_LEN] = { CH_NULL };
 double NUM_QUEUE[QUEUE_LEN] = { 0.0 };
