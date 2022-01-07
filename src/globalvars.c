a715cb0937f2b04652d43fcee5d67acbf1306741
OogleGlu <avirukbasak@gmail.com>
2022-01-07 14:07:05 +0530

    added queues

--- src/globalvars.c
+++ src/globalvars.c
@@ -4,3 +4,6 @@ const char *VALID_CHARS = "0123456789( .^/%*+-)";
 
 char EXPRESSION[STRLEN] = { CH_NULL };
 char STR_TOKEN[STRLEN] = { CH_NULL };
+
+double NUM_QUEUE[QUEUE_LEN] = { 0.0 };
+byte OP_QUEUE[QUEUE_LEN] = { CH_NULL };
